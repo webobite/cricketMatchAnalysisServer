c60f2727a143ccab19d539e06585832a8f58b6f2
webobite <singhsubham510@gmail.com>
2020-07-06 20:54:05 +0530

    Added analysis route

--- routes/main.route.js
+++ routes/main.route.js
@@ -33,4 +33,39 @@ router.post('/upload-csv', upload.single('file'), function async(req, res) {
         })
       })
   });
+
+  /**
+   * Route to analyse and Find answer of following questions : 
+   *  - Top 5 T20 Batsman : Virat, Rohit, Shikhar, Sachin
+   *  - Top 5(Highest Average in tests) : Dravid, P2, P3, P4, P5
+   *  - Top 5 (Total Runs in one day): Sachin, Virat, P3, P4, P5
+   */
+  router.post('/analyse', upload.single('file'), function async(req, res) {
+    csv().fromFile(req.file.path)
+      .then((jsonObj) => {
+        console.log("Json Obj : : : ", jsonObj);
+        fs.unlinkSync(req.file.path); 
+        var query = {
+            query1 : [],
+            query2 : [],
+            query3 : []
+        }  // remove temp file process "fileRows" and respond
+        res.send({
+          success: true,
+          data: query,
+          message : {
+              query1 : "Top 5 T20 Batsman",
+              query2 : "Top 5(Highest Average in tests)",
+              query3 : "Top 5 (Total Runs in one day)"
+          }
+        })
+      })
+      .catch((err) => {
+        console.log("Error : : : ", err);
+        res.send({
+          success: false,
+          data: err
+        })
+      })
+  });
 module.exports = router
